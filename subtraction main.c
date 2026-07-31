eb87338412c311500cf40f9d7f8c0545b83e491d
Sanjana More <sg.more0708@gmail.com>
2026-07-31 19:50:45 +0530

    Update subtraction main.c

--- subtraction main.c
+++ subtraction main.c
@@ -1,21 +1,12 @@
 #include <stdio.h>
 int main()
 {
-
 int X,Y,Z;
-
 printf("\n enter the first no:");
-
 scanf("%d",&X);
-
 printf("\n enter the second no:");
-
 scanf("%d",&Y);
-
 Z=X-Y;
-
 printf("\n the result of subtraction is %d",Z);
-
 return 0;
-
 }
